2bed238187e14b7b1a5fcf757a17b6ffee37b47b
badapple21 <mjn2024@gmail.com>
2022-08-13 00:21:11 -0400

    worked on ps4a

---
+++ 6.0001/ps4/ps4a.py
@@ -0,0 +1,61 @@
+# Problem Set 4A
+# Name: <your name here>
+# Collaborators:
+# Time Spent: x:xx
+
+def get_permutations(sequence):
+    '''
+    Enumerate all permutations of a given string
+
+    sequence (string): an arbitrary string to permute. Assume that it is a
+    non-empty string.  
+
+    You MUST use recursion for this part. Non-recursive solutions will not be
+    accepted.
+
+    Returns: a list of all permutations of sequence
+
+    Example:
+    >>> get_permutations('abc')
+    ['abc', 'acb', 'bac', 'bca', 'cab', 'cba']
+
+    Note: depending on your implementation, you may return the permutations in
+    a different order than what is listed here.
+    '''
+    possiblites = []
+
+    if(len(sequence)==1):
+        return [sequence]
+    else:
+        letter = sequence[0]
+        sequence = sequence[1:]
+        for possiblity in get_permutations(sequence):
+            for i in range(len(sequence)):
+                possiblities.append(insert_letter(i, sequence, letter))
+
+    return possiblites
+
+def insert_letter(place, string, letter):
+    if(place==0): 
+        return f"{letter}{string}"
+    else:
+        return f"{string[:place]}{letter}{string[place:]}"
+
+
+if __name__ == '__main__':
+#    #EXAMPLE
+#    example_input = 'abc'
+#    print('Input:', example_input)
+#    print('Expected Output:', ['abc', 'acb', 'bac', 'bca', 'cab', 'cba'])
+#    print('Actual Output:', get_permutations(example_input))
+    
+#    # Put three example test cases here (for your sanity, limit your inputs
+#    to be three characters or fewer as you will have n! permutations for a 
+#    sequence of length n)
+    
+#unit test for insert_letter
+#   print(insert_letter(0, "ust", "b"))
+#   print(insert_letter(1, "ust", "b"))
+#   print(insert_letter(2, "ust", "b"))
+
+    print(get_permutations("abc"))
